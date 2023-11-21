bd10596c24a53ed9871ce40187abc87ec2f2d761
Juan C Lopez <juancalpz@yahoo.com>
2023-11-21 10:41:48 -0400

    Changed functions names and added 'i' and 'd' functions

--- functions.c
+++ functions.c
@@ -1,12 +1,10 @@
 #include "main.h"
-/**
- *_pchar - prints a char
- *
+/*e
+ *_char - prints a char
  *@c: char to print
- *
  * Return: Char, 0 Success
  **/
-int _pchar(char c)
+int _char(char c)
 {
 	if (c != '\0')
 	{
@@ -17,13 +15,11 @@ int _pchar(char c)
 	return (0);
 }
 /**
- * _pstr - Functions to print strings
- *
+ * _str - Functions to print strings
  * @s: parameter
- *
  * Return: String
  **/
-int _pstr(char *s)
+int _str(char *s)
 {
 	int ln;
 
@@ -42,15 +38,73 @@ int _pstr(char *s)
 		return (0);
 }
 /**
- * _pdef - Print %
- *
+ * _def - Print %
  * @c: character to print
- *
  * Return: % or Num Success
  **/
-int _pdef(char c)
+int _def(char c)
 {
 	write(1, "%", 1);
 	write(1, &c, 1);
 	return (2);
+}
+/**
+ * _int - Prints an in or dec
+ * @num: number to print
+ * Return: Printed number
+ **/
+int _int(int num)
+{
+	char n[10];
+	int i, c = 0, neg = 0;
+
+	if (num < 10)
+	{
+		write (1, "-", 1);
+		neg++;
+		num *= (-1);
+	}
+	for (i = 9; num != 0; i--)
+	{
+		n[i] = '0' + (num % 10);
+		num /= 10;
+	}
+	i++;
+	c = 10 - i;
+	write (1, &n[i], c);
+	return (neg + c);
+}
+/**
+ * _uns - prints an unsigned int
+ * @n: variable to determine if unsigned
+ * Return: Unsigned int
+ **/
+int _uns(unsigned int n)
+{
+	int count = 0;
+
+	do
+	{
+		char digit = '0' + n % 10;
+		write(1, &digit, 1);
+		n /= 10;
+		count++;
+	} 
+	while (n != 0);
+	return (count);
+}
+/**
+ * _strlen - calculate the lenght of a string
+ * @s: integer used
+ * Return: returns the lenght of the string
+ */
+int _strlen(char *s)
+{
+	int i, lenght = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		lenght++;
+	}
+	return (lenght);
 }
